56548bfd648c4316b425f9799ee271e635103a43
Filip Smoleński <ps394742@mimuw.edu.pl>
2018-12-02 16:29:44 +0100

    squares + fixed attack

---
+++ battle.cc
@@ -0,0 +1,24 @@
+#pragma once
+#include<iostream>
+#include<cassert>
+#include<array>
+#include"rebelfleet.h"
+#include"imperialfleet.h"
+using namespace std;
+
+
+template<typename T, T end, T cur = 0, T...results>
+constexpr static auto squares() {
+	if constexpr (cur * cur <= end)
+		return squares<T, end, cur + 1, results..., cur * cur>();
+	else
+		return array<T, sizeof...(results)> ({results...});
+}
+
+template<typename T, T t0, T t1, typename...S>
+class SpaceBattle {
+	static_assert(t0 >=0 && t1 >= t0);
+	static constexpr auto BattleTimes = squares<T, t1>();
+
+	
+};
--- rebelfleet.h
+++ rebelfleet.h
@@ -1,5 +1,9 @@
+#ifndef REBELFLEET_H
+#define REBELFLEET_H
+
 #include<iostream>
 #include<cassert>
+
 using namespace std;
 
 constexpr int MIN_STARCRUISER_SPEED = 99999;
@@ -7,20 +11,25 @@ constexpr int MAX_STARCRUISER_SPEED = 299795;
 constexpr int MIN_XWING_EXPLORER_SPEED = 299796;
 constexpr int MAX_XWING_EXPLORER_SPEED = 2997960;
 
-enum RebelSpaceshipType {explorer, starCruiser, xWing};
-
-template<typename U, RebelSpaceshipType spaceshipType, int minSpeed, int maxSpeed>
+template<typename U, bool canAttack, int minSpeed, int maxSpeed>
 class RebelStarship {
     U shield;
     U speed;
     U attackPower;
+    constexpr static bool attackShip = canAttack;
+
+    template<typename I, typename R>
+	friend void attack(I &imperialShip, R &rebelShip);
+
 public:
     using valueType = U;
 
+    template <bool A = canAttack, typename = std::enable_if_t<!A>>
     RebelStarship(U shield, U speed): shield(shield), speed(speed) {
         assert(speed <= maxSpeed && speed >= minSpeed);
     }
 
+    template <bool A = canAttack, typename = std::enable_if_t<A>>
     RebelStarship(U shield, U speed, U attackPower): shield(shield), speed(speed), attackPower(attackPower) {
         assert(speed <= maxSpeed && speed >= minSpeed);
     }
@@ -37,21 +46,19 @@ public:
         shield = damage > shield ? 0 : shield - damage;
     }
 
-    bool canAttack() {
-        return spaceshipType != explorer;
-    }
-
-    template <RebelSpaceshipType A = spaceshipType, typename = typename std::enable_if<spaceshipType != explorer>>
+    template <bool A = canAttack, typename = std::enable_if_t<A>>
     U getAttackPower() {
         return attackPower;
     }
 };
 
 template<typename U>
-using XWing = RebelStarship<U, xWing, MIN_XWING_EXPLORER_SPEED, MIN_XWING_EXPLORER_SPEED>;
+using XWing = RebelStarship<U, true, MIN_XWING_EXPLORER_SPEED, MIN_XWING_EXPLORER_SPEED>;
 
 template<typename U>
-using StarCruiser = RebelStarship<U, starCruiser, MIN_STARCRUISER_SPEED, MAX_STARCRUISER_SPEED>;
+using StarCruiser = RebelStarship<U, true, MIN_STARCRUISER_SPEED, MAX_STARCRUISER_SPEED>;
 
 template<typename U>
-using Explorer = RebelStarship<U, explorer, MIN_XWING_EXPLORER_SPEED, MAX_XWING_EXPLORER_SPEED>;
+using Explorer = RebelStarship<U, false, MIN_XWING_EXPLORER_SPEED, MAX_XWING_EXPLORER_SPEED>;
+
+#endif
